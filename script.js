3a87b44c52bb240e2f16d97b3cc3bbe9b46c7f55
Aniket Botre <aniketbotre007@gmail.com>
2022-12-03 12:11:24 +0530

    Week 3 project

---
+++ script.js
@@ -0,0 +1,72 @@
+"use strict";
+
+//Deciding the winner of the single round
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    }else if(playerSelection === "rock" && computerSelection === "scissors"){
+        return true;
+    }else if(playerSelection === "scissors" && computerSelection === "rock"){
+        return false;
+    }else if(playerSelection === "rock" && computerSelection === "paper"){
+        return false;
+    }else if(playerSelection === "paper" && computerSelection === "rock"){
+        return true;
+    }else if(playerSelection === "scissors" && computerSelection === "paper"){
+        return true;
+    }else if(playerSelection === "paper" && computerSelection === "scissors"){
+        return false;
+    }
+}
+
+//Generating computer input selection
+function computerPlay() {
+    let randomizer = Math.floor(Math.random() * 3); 
+    let randomSelection = ["rock", "paper", "scissors"][randomizer];
+    return randomSelection;
+}
+
+//Storing computer input selection
+const computerSelection = computerPlay();
+
+
+function game() {
+    let playerScore = 0;
+    let botScore = 0;
+
+    //Looping the game for 5 rounds
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        if(playerSelection !==  'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+            console.log(`Please enter a valid input: 'rock', 'paper', or 'scissors'`);
+            break;
+        }
+        let result = playRound(playerSelection, computerSelection);
+
+        //
+        if(result === 'Tie'){
+            console.log("It's a tie");
+        }else if(result){
+            console.log(`You have won!🥳 ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
+        }else{
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            botScore++;
+        }
+        console.log(`Player score: ${playerScore} |\tComputer score: ${botScore}`);
+    }
+    //Printing overall results to the console
+    console.log(`\nPlayer's overall score: ${playerScore} |\tComputer's overall score: ${botScore}`);
+
+    //Declaring the winner
+    if(playerScore > botScore){
+        console.log('Player have won🎊🎊');
+    }else if(playerScore == botScore){
+        console.log('Scores have been tied!!!');
+    }else{
+        console.log('Computer have won');
+    }
+}
+
+//Calling the game function
+game();
